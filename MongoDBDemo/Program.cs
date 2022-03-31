062241618ad58c918a8baf2ae134a4482c8f40b5
marcelo-sl <mar.sousa2061@gmail.com>
2022-03-31 15:53:49 -0300

    MongoDBDemo

---
+++ MongoDBDemo/Program.cs
@@ -0,0 +1,25 @@
+﻿using MongoDataAccess.DataAccess;
+using MongoDB.Driver;
+using MongoDataAccess.Models;
+
+ChoreDataAccess db = new ChoreDataAccess();
+
+await db.CreateUser(new UserModel { FirstName = "Marcelo", LastName = "Lima" });
+
+var users = await db.GetAllUsers();
+
+var chore = new ChoreModel() 
+{ 
+    AssignedTo = users.First(), 
+    ChoreText = "Mow the Lawn", 
+    FrequencyInDays = 7 
+};
+
+await db.CreateChore(chore);
+
+var chores = await db.GetAllChores();
+
+var newChore = chores.First();
+newChore.LastCompleted = DateTime.Now;
+
+await db.CompleteChore(newChore);
